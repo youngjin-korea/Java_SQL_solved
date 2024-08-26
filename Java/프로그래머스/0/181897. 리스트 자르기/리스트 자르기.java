44da26fde987afacf6706c88f7d5525efe3bff53
YoungJinKim <youngjin1293@naver.com>
2024-08-26 14:04:11 +0900

    [level 0] Title: 리스트 자르기, Time: 0.05 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> result = new ArrayList();
+        
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        
+        switch(n){
+            case 1:
+                for(int i=0; i<=b; i++){
+                   result.add(num_list[i]);
+                }
+                break;
+            case 2:
+                for(int i=a; i<num_list.length; i++){
+                   result.add(num_list[i]);
+                }
+                break;
+            case 3:
+                for(int i=a; i<=b; i++){
+                   result.add(num_list[i]);
+                }
+                break;
+            case 4:
+                for(int i=a; i<=b; i+=c){
+                   result.add(num_list[i]);
+                }
+                break;
+        }
+        
+        return result;
+    }
+}
