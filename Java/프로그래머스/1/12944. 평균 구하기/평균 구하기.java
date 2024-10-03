3355587b1afe3104560a4797057dc4cc8d2da93c
YoungJinKim <youngjin1293@naver.com>
2024-10-03 15:31:53 +0900

    [level 1] Title: 평균 구하기, Time: 9.76 ms, Memory: 85.3 MB -BaekjoonHub

--- Java/프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ Java/프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,17 +1,7 @@
+import java.util.*;
+
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
-        
-        
-    double sum = 0;
-    
-    for(int item : arr){
-        sum += item;
-    }
-    
-    answer = sum / arr.length;
-    
-    return answer;
-        
+        return Arrays.stream(arr).average().orElse(0.0);
     }
 }
