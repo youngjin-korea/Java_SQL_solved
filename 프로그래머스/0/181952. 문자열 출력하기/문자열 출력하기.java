ea3c459b05b9ad1623fbfcdf7c686c69f3ab9de1
YoungJinKim <youngjin1293@naver.com>
2025-01-01 23:03:53 +0900

    [level 0] Title: 문자열 출력하기, Time: 162.60 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*; 
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        /*Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);*/
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in));
+        String answer =  br.readLine();
+        System.out.println(answer);
+        
+    }
+}
