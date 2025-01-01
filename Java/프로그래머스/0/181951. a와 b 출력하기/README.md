dd15415a6ae531633f645b6e583f74de903ec792
YoungJinKim <youngjin1293@naver.com>
2025-01-02 08:03:50 +0900

    [level 0] Title: a와 b 출력하기, Time: 143.18 ms, Memory: 62.6 MB -BaekjoonHub

--- Java/프로그래머스/0/181951. a와 b 출력하기/README.md
+++ Java/프로그래머스/0/181951. a와 b 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] a와 b 출력하기 - 181951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181951?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181951) 
 
 ### 성능 요약
 
-메모리: 66 MB, 시간: 134.29 ms
+메모리: 62.6 MB, 시간: 143.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 20일 13:37:36
+2025년 01월 02일 08:04:48
 
 ### 문제 설명
 
--- Java/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
+++ Java/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -1,12 +1,15 @@
-import java.util.Scanner;
-
+import java.io.*;
+import java.util.*;
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        System.out.println("a = "+a);
-         System.out.println("b = "+b);
+    public static void main (String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String input = bf.readLine();
+        
+        StringTokenizer st = new StringTokenizer(input);
+        
+        String a = st.nextToken();
+        String b = st.nextToken();
+        
+        System.out.printf("a = %s \nb = %s", a, b);
     }
 }
