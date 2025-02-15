fdd7210514686e48798807b35d7a049c08b1c437
YoungJinKim <youngjin1293@naver.com>
2025-02-15 21:23:40 +0900

    [level 0] Title: 문자열 출력하기, Time: 127.40 ms, Memory: 69.3 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/README.md
+++ 프로그래머스/0/181952. 문자열 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 출력하기 - 181952 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181952) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181952?language=java) 
 
 ### 성능 요약
 
-메모리: 70.2 MB, 시간: 162.60 ms
+메모리: 69.3 MB, 시간: 127.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 01일 23:03:48
+2025년 02월 15일 21:23:32
 
 ### 문제 설명
 
--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -1,15 +1,9 @@
+import java.io.*;
 import java.util.*;
-import java.io.*; 
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        /*Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        System.out.println(a);*/
-        BufferedReader br = new BufferedReader(
-            new InputStreamReader(System.in));
-        String answer =  br.readLine();
-        System.out.println(answer);
-        
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(bf.readLine());
     }
 }
