3c99721a551304d58c273515ef4a652ea1d16d9f
YoungJinKim <youngjin1293@naver.com>
2025-02-15 21:33:18 +0900

    [level 0] Title: 문자열 뒤집기, Time: 15.80 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuffer sb = new StringBuffer(my_string.substring(s, e+1));
+        sb.reverse();
+        
+        return my_string.substring(0, s) + sb.toString() + my_string.substring(e+1);
+    }
+}
