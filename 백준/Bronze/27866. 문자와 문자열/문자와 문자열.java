6a32690c78af37a3c891c19cb74cdd5b81a1a71d
YoungJinKim <youngjin1293@naver.com>
2025-02-16 22:03:10 +0900

    [Bronze V] Title: 문자와 문자열, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int i = sc.nextInt()-1;
+        System.out.println(s.charAt(i));
+    } 
+}
